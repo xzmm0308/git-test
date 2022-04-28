224d50d9aea1f4221949796bf5ccb57b19e5bb17
xzm <xzm@qq.com>
2022-04-28 21:25:14 +0800

    first commit

---
+++ src/main/java/com/xzm/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xzm.git;
+
+/**
+ * AUTHOR:xzm
+ * DATE:2022/4/28 21:20
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
